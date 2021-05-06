ebc5a32c93705ea32fedd2a7fe7895578ea6609c
FaikKarazade <faik@mobisem.com>
2021-05-06 19:27:37 +0300

    Initial Commit

---
+++ United Space Travelers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  United Space Travelers
+//
+//  Created by Faiq on 6.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
